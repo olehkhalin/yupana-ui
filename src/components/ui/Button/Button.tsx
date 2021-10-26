8ab12122da5af806f1205227041a83ab287ac148
svyatoslavtt <svyatoslavtt@madfish.solutions>
2021-10-26 13:19:21 +0300

    Add buttons

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import cx from 'classnames';
+
+import { ReactComponent as Arrow } from 'svg/Arrow.svg';
+
+import s from './Button.module.sass';
+
+export type HTMLButtonType = (
+  | React.HTMLProps<HTMLButtonElement>
+  | React.HTMLProps<HTMLAnchorElement>
+);
+
+type ButtonProps = {
+  theme?: keyof typeof themeClass
+  sizeT?: keyof typeof sizeClass
+  action?: 'supply' | 'borrow'
+  withArrow?: boolean
+  external?: boolean
+  className?: string
+  activeClassName?: string
+} & HTMLButtonType;
+
+const actionClass = {
+  supply: s.supply,
+  borrow: s.borrow,
+};
+
+const themeClass = {
+  primary: s.primary,
+  secondary: s.secondary,
+  tertiary: s.tertiary,
+  light: s.light,
+  accent: s.accent,
+  clear: s.clear,
+};
+
+const sizeClass = {
+  large: s.large,
+  medium: s.medium,
+  small: s.small,
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  theme = 'primary',
+  sizeT = 'large',
+  type = 'button',
+  action = 'supply',
+  withArrow = false,
+  href,
+  external = false,
+  className,
+  activeClassName,
+  children,
+  ...props
+}) => {
+  const classNames = cx(
+    s.root,
+    themeClass[theme],
+    sizeClass[sizeT],
+    { [s.withArrow]: (withArrow && (theme === 'light' || theme === 'accent')) },
+    actionClass[action],
+    className,
+  );
+
+  const inner = (
+    <>
+      {children}
+      {
+        (withArrow && (theme === 'light' || theme === 'accent'))
+        && <Arrow className={s.arrow} />
+      }
+    </>
+  );
+
+  if (href) {
+    if (external) {
+      return (
+        <a
+          href={href}
+          target="_blank"
+          rel="noreferrer noopener"
+          className={classNames}
+          {...(props as React.HTMLProps<HTMLAnchorElement>)}
+        >
+          {inner}
+        </a>
+      );
+    }
+    return (
+      <NavLink
+        to={href}
+        className={classNames}
+        activeClassName={s.activeClassName}
+      >
+        {inner}
+      </NavLink>
+    );
+  }
+
+  return (
+    <button
+      // @ts-ignore
+      // eslint-disable-next-line react/button-has-type
+      type={type}
+      className={classNames}
+      {...props}
+    >
+      {inner}
+    </button>
+  );
+};
--- src/pages/ui-kit/UiKit.tsx
+++ src/pages/ui-kit/UiKit.tsx
@@ -1,10 +1,760 @@
-import Container from 'components/common/Container';
 import React from 'react';
+
+import { Button } from 'components/ui/Button';
+import Container from 'components/common/Container';
 
 import s from './UiKit.module.sass';
 
 export const UiKit: React.FC = () => (
   <Container className={s.root}>
-    UiKit
+    <div className={s.block}>
+      <div className={s.buttonsBlock}>
+        <div className={s.buttonAction}>
+          Supply
+        </div>
+        <div className={s.buttons}>
+          <Button
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            sizeT="medium"
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            sizeT="small"
+          >
+            Button text
+          </Button>
+        </div>
+        <div className={s.buttons}>
+          <Button
+            className={s.button}
+            disabled
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            sizeT="medium"
+            disabled
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            sizeT="small"
+            disabled
+          >
+            Button text
+          </Button>
+        </div>
+      </div>
+      <div className={s.buttonsBlock}>
+        <div className={s.buttons}>
+          <Button
+            className={s.button}
+            theme="secondary"
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="secondary"
+            sizeT="medium"
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="secondary"
+            sizeT="small"
+          >
+            Button text
+          </Button>
+        </div>
+        <div className={s.buttons}>
+          <Button
+            className={s.button}
+            theme="secondary"
+            disabled
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="secondary"
+            sizeT="medium"
+            disabled
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="secondary"
+            sizeT="small"
+            disabled
+          >
+            Button text
+          </Button>
+        </div>
+      </div>
+      <div className={s.buttonsBlock}>
+        <div className={s.buttons}>
+          <Button
+            className={s.button}
+            theme="tertiary"
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="tertiary"
+            sizeT="medium"
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="tertiary"
+            sizeT="small"
+          >
+            Button text
+          </Button>
+        </div>
+        <div className={s.buttons}>
+          <Button
+            className={s.button}
+            theme="tertiary"
+            disabled
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="tertiary"
+            sizeT="medium"
+            disabled
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="tertiary"
+            sizeT="small"
+            disabled
+          >
+            Button text
+          </Button>
+        </div>
+      </div>
+      <div className={s.buttonsBlock}>
+        <div className={s.buttons}>
+          <Button
+            className={s.button}
+            theme="light"
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="light"
+            sizeT="medium"
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="light"
+            sizeT="small"
+          >
+            Button text
+          </Button>
+        </div>
+        <div className={s.buttons}>
+          <Button
+            className={s.button}
+            theme="light"
+            disabled
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="light"
+            sizeT="medium"
+            disabled
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="light"
+            sizeT="small"
+            disabled
+          >
+            Button text
+          </Button>
+        </div>
+      </div>
+      <div className={s.buttonsBlock}>
+        <div className={s.buttons}>
+          <Button
+            className={s.button}
+            theme="accent"
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="accent"
+            sizeT="medium"
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="accent"
+            sizeT="small"
+          >
+            Button text
+          </Button>
+        </div>
+        <div className={s.buttons}>
+          <Button
+            className={s.button}
+            theme="accent"
+            disabled
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="accent"
+            sizeT="medium"
+            disabled
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="accent"
+            sizeT="small"
+            disabled
+          >
+            Button text
+          </Button>
+        </div>
+      </div>
+      <div className={s.buttonsBlock}>
+        <div className={s.buttons}>
+          <Button
+            className={s.button}
+            theme="light"
+            withArrow
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="light"
+            sizeT="medium"
+            withArrow
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="light"
+            sizeT="small"
+            withArrow
+          >
+            Button text
+          </Button>
+        </div>
+        <div className={s.buttons}>
+          <Button
+            className={s.button}
+            theme="light"
+            withArrow
+            disabled
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="light"
+            sizeT="medium"
+            withArrow
+            disabled
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="light"
+            sizeT="small"
+            withArrow
+            disabled
+          >
+            Button text
+          </Button>
+        </div>
+      </div>
+      <div className={s.buttonsBlock}>
+        <div className={s.buttons}>
+          <Button
+            className={s.button}
+            theme="accent"
+            withArrow
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="accent"
+            sizeT="medium"
+            withArrow
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="accent"
+            sizeT="small"
+            withArrow
+          >
+            Button text
+          </Button>
+        </div>
+        <div className={s.buttons}>
+          <Button
+            className={s.button}
+            theme="accent"
+            withArrow
+            disabled
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="accent"
+            sizeT="medium"
+            withArrow
+            disabled
+          >
+            Button text
+          </Button>
+          <Button
+            className={s.button}
+            theme="accent"
+            sizeT="small"
+            withArrow
+            disabled
+          >
+            Button text
+          </Button>
+        </div>
+      </div>
+    </div>
+    <div className={s.block}>
+      <div className={s.buttonsBlock}>
+        <div className={s.buttonAction}>
+          Borrow
+        </div>
+        <div className={s.buttons}>
+          <Button
+            action="borrow"
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            sizeT="medium"
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            sizeT="small"
+            className={s.button}
+          >
+            Button text
+          </Button>
+        </div>
+        <div className={s.buttons}>
+          <Button
+            action="borrow"
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            sizeT="medium"
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            sizeT="small"
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+        </div>
+      </div>
+      <div className={s.buttonsBlock}>
+        <div className={s.buttons}>
+          <Button
+            action="borrow"
+            theme="secondary"
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="secondary"
+            sizeT="medium"
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="secondary"
+            sizeT="small"
+            className={s.button}
+          >
+            Button text
+          </Button>
+        </div>
+        <div className={s.buttons}>
+          <Button
+            action="borrow"
+            theme="secondary"
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="secondary"
+            sizeT="medium"
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="secondary"
+            sizeT="small"
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+        </div>
+      </div>
+      <div className={s.buttonsBlock}>
+        <div className={s.buttons}>
+          <Button
+            action="borrow"
+            theme="tertiary"
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="tertiary"
+            sizeT="medium"
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="tertiary"
+            sizeT="small"
+            className={s.button}
+          >
+            Button text
+          </Button>
+        </div>
+        <div className={s.buttons}>
+          <Button
+            action="borrow"
+            theme="tertiary"
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="tertiary"
+            sizeT="medium"
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="tertiary"
+            sizeT="small"
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+        </div>
+      </div>
+      <div className={s.buttonsBlock}>
+        <div className={s.buttons}>
+          <Button
+            action="borrow"
+            theme="light"
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="light"
+            sizeT="medium"
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="light"
+            sizeT="small"
+            className={s.button}
+          >
+            Button text
+          </Button>
+        </div>
+        <div className={s.buttons}>
+          <Button
+            action="borrow"
+            theme="light"
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="light"
+            sizeT="medium"
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="light"
+            sizeT="small"
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+        </div>
+      </div>
+      <div className={s.buttonsBlock}>
+        <div className={s.buttons}>
+          <Button
+            action="borrow"
+            theme="accent"
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="accent"
+            sizeT="medium"
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="accent"
+            sizeT="small"
+            className={s.button}
+          >
+            Button text
+          </Button>
+        </div>
+        <div className={s.buttons}>
+          <Button
+            action="borrow"
+            theme="accent"
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="accent"
+            sizeT="medium"
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="accent"
+            sizeT="small"
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+        </div>
+      </div>
+      <div className={s.buttonsBlock}>
+        <div className={s.buttons}>
+          <Button
+            action="borrow"
+            theme="light"
+            withArrow
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="light"
+            sizeT="medium"
+            withArrow
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="light"
+            sizeT="small"
+            withArrow
+            className={s.button}
+          >
+            Button text
+          </Button>
+        </div>
+        <div className={s.buttons}>
+          <Button
+            action="borrow"
+            theme="light"
+            withArrow
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="light"
+            sizeT="medium"
+            withArrow
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="light"
+            sizeT="small"
+            withArrow
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+        </div>
+      </div>
+      <div className={s.buttonsBlock}>
+        <div className={s.buttons}>
+          <Button
+            action="borrow"
+            theme="accent"
+            withArrow
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="accent"
+            sizeT="medium"
+            withArrow
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="accent"
+            sizeT="small"
+            withArrow
+            className={s.button}
+          >
+            Button text
+          </Button>
+        </div>
+        <div className={s.buttons}>
+          <Button
+            action="borrow"
+            theme="accent"
+            withArrow
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="accent"
+            sizeT="medium"
+            withArrow
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+          <Button
+            action="borrow"
+            theme="accent"
+            sizeT="small"
+            withArrow
+            disabled
+            className={s.button}
+          >
+            Button text
+          </Button>
+        </div>
+      </div>
+    </div>
   </Container>
 );
