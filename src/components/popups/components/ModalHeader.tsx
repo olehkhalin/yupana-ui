b8e4a5f8fb42ec2d0aeb53be66bb84dd79527ab5
Artem <violence044@gmail.com>
2021-11-18 11:33:33 +0300

    Add ModalHeader

---
+++ src/components/popups/components/ModalHeader.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import cx from 'classnames';
+
+import s from './ModalHeader.module.sass';
+
+type ModalHeaderProps = {
+  title: string
+  description: string
+  className?: string
+};
+
+export const ModalHeader: React.FC<ModalHeaderProps> = ({
+  title,
+  description,
+  className,
+}) => (
+  <div className={cx(s.root, className)}>
+    <h2 className={s.title}>
+      {title}
+    </h2>
+    {description}
+  </div>
+);
