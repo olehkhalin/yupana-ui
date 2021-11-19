cf14c09308e44d830a4b2bec4d336f581a50e3e1
Artem <violence044@gmail.com>
2021-11-19 16:02:13 +0300

    Fix after review

--- src/components/popups/Account/Account.tsx
+++ src/components/popups/Account/Account.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import { Modal } from 'components/ui/Modal';
-import { Button } from 'components/ui/Button';
 import { shortize } from 'utils/getShortize';
 import { useMphoneOrWider } from 'utils/getMediaQuery';
+import { Modal } from 'components/ui/Modal';
+import { Button } from 'components/ui/Button';
 import { ReactComponent as IconCopy } from 'svg/IconCopy.svg';
 import { ReactComponent as IconLink } from 'svg/IconLink.svg';
+import { ModalHeader } from '../components';
 
 import s from './Account.module.sass';
 
@@ -29,42 +30,39 @@ export const Account: React.FC<AccountProps> = ({
     onRequestClose={onRequestClose}
     innerClassName={s.inner}
   >
-    <div className={s.root}>
+    <Button
+      theme="clear"
+      className={s.logout}
+    >
+      log out
+    </Button>
+    <ModalHeader
+      title={title}
+      description={description}
+      className={s.root}
+    />
+    <div className={s.address}>
+      {useMphoneOrWider() ? address : shortize(address)}
+    </div>
+    <div className={s.options}>
       <Button
         theme="clear"
-        className={s.logout}
+        className={s.button}
       >
-        log out
+        <p className={s.optionName}>
+          View on explorer
+        </p>
+        <IconLink />
       </Button>
-      <h2 className={s.title}>
-        {title}
-      </h2>
-      <div className={s.description}>
-        {description}
-      </div>
-      <div className={s.address}>
-        {useMphoneOrWider() ? address : shortize(address)}
-      </div>
-      <div className={s.options}>
-        <div className={s.option}>
-          <Button
-            theme="clear"
-            className={s.button}
-          >
-            <p className={s.optionName}>View on explorer</p>
-            <IconCopy />
-          </Button>
-        </div>
-        <div className={s.option}>
-          <Button
-            theme="clear"
-            className={s.button}
-          >
-            <p className={s.optionName}>Copy Address</p>
-            <IconLink />
-          </Button>
-        </div>
-      </div>
+      <Button
+        theme="clear"
+        className={s.button}
+      >
+        <p className={s.optionName}>
+          Copy Address
+        </p>
+        <IconCopy />
+      </Button>
     </div>
   </Modal>
 );
