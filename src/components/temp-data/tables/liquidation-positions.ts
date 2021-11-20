acc3c91cd6e4a6653eace52cefee9c6b4bbdaa13
svyatoslavtt <svyatoslavtt@madfish.solutions>
2021-11-20 17:12:43 +0200

    Add desktop pagination

--- src/components/temp-data/tables/liquidation-positions.ts
+++ src/components/temp-data/tables/liquidation-positions.ts
@@ -55,11 +55,4 @@ export const LIQUIDATION_POSITIONS_DATA = [
     healthFactor: 1.04,
     borrowerAddress: 'tz3QT9dHYKDqh563chVa6za8526ys1UKfRfL',
   },
-  {
-    totalBorrowed: 2062.34,
-    borrowedAsset: ['XTZ', 'tzBTC'],
-    collateralAsset: ['USDc', 'tzBTC'],
-    healthFactor: 1.04,
-    borrowerAddress: 'tz3QT9dHYKDqh563chVa6za8526ys1UKfRfL',
-  },
 ];
