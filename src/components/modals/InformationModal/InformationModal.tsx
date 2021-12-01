60a49f402f377463a6288f00358da059644c1bc6
svyatoslavtt <svyatoslavtt@madfish.solutions>
2021-12-01 15:00:29 +0200

    Add onRequestClose on button

--- src/components/modals/InformationModal/InformationModal.tsx
+++ src/components/modals/InformationModal/InformationModal.tsx
@@ -26,6 +26,7 @@ export const InformationModal: React.FC<InformationModalProps> = ({
     />
     <Button
       sizeT="small"
+      onClick={onRequestClose}
     >
       {buttonText}
     </Button>
