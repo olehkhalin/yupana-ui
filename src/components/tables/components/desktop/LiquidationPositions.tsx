188578dd7c2ba1278f70681f2b04d5f6efa264c1
svyatoslavtt <svyatoslavtt@madfish.solutions>
2021-12-06 11:29:58 +0200

    Add some fix

--- src/components/tables/components/desktop/LiquidationPositions.tsx
+++ src/components/tables/components/desktop/LiquidationPositions.tsx
@@ -1,7 +1,6 @@
 import React, { useMemo } from 'react';
 import cx from 'classnames';
 
-import { AppRoutes } from 'routes/main-routes';
 import { getPrettyAmount } from 'utils/getPrettyAmount';
 import { shortize } from 'utils/getShortize';
 import { getPrettyPercent } from 'utils/getPrettyPercent';
