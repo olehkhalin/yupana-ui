d7752b7e54b7e16c752bd29d5b89a2399da0c744
svyatoslavtt <svyatoslavtt@madfish.solutions>
2022-01-17 16:02:47 +0200

    Add constant

--- src/utils/helpers/amount/convertTokenPrice.ts
+++ src/utils/helpers/amount/convertTokenPrice.ts
@@ -1,5 +1,6 @@
 import BigNumber from 'bignumber.js';
+import { ORACLE_PRICE_PRECISION } from 'constants/default';
 
 export const convertTokenPrice = (price: BigNumber, decimals: BigNumber): BigNumber => (
-  price.div(1e24).times(decimals)
+  price.div(`1e${ORACLE_PRICE_PRECISION}`).times(decimals)
 );
