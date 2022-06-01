b0e242efd192b9212d922a89b744f174d0ecdd2c
olehkhalin <olehkhalin@gmail.com>
2022-06-01 00:05:59 -0400

    Edited repay with tez

--- src/utils/dapp/methods/repay.ts
+++ src/utils/dapp/methods/repay.ts
@@ -1,6 +1,7 @@
-import { TezosToolkit } from "@taquito/taquito";
+import { ContractMethod, TezosToolkit, Wallet } from "@taquito/taquito";
 import BigNumber from "bignumber.js";
 
+import { WTEZ_CONTRACT } from "constants/defaults";
 import { batchify } from "utils/dapp/helpers";
 import { commonMethods, CommonParams } from "./commonMethods";
 
@@ -14,6 +15,13 @@ export const repay = async (
   params: RepayProps
 ) => {
   const { fabricaContractAddress, yToken, amount, isMaxAmount } = params;
+
+  const prevMethods: ContractMethod<Wallet>[] = [];
+
+  if (params.tokenContract === WTEZ_CONTRACT) {
+    const wtezContract = await tezos.wallet.at(WTEZ_CONTRACT);
+    prevMethods.push(wtezContract.methods.mint(accountPkh, [["unit"]]));
+  }
 
   const fabricaContract = await tezos.wallet.at(fabricaContractAddress);
   const deadline = (
@@ -35,10 +43,16 @@ export const repay = async (
     method: mainMethod,
   });
 
-  batchify(
-    batch,
-    methods.map((method) => method.toTransferParams({ storageLimit: 460 }))
-  );
+  batchify(batch, [
+    ...prevMethods.map((method) =>
+      method.toTransferParams({
+        storageLimit: 460,
+        amount: +params.amount!,
+        mutez: true,
+      })
+    ),
+    ...methods.map((method) => method.toTransferParams({ storageLimit: 460 })),
+  ]);
 
   return batch.send();
 };
